e2faac6352d3c07eb5591305dff169e1e0e33b89
leonardoddev <leonardo.devicari2@gmail.com>
2024-08-27 15:39:57 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+});
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+});
+
+$('#cep').mask('00000-000', {
+    placeholder: '01234-567'
+});
+
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        endereco: {
+            required: true
+        },
+        cep: {
+            required: true
+        },
+        cpf: {
+            required: true
+        },
+    },
+    messages: {
+        nome: "Por favor, informe seu nome",
+        email: {
+            required: "Por favor, informe seu email",
+            email: "Por favor, informe um email válido"
+        },
+        telefone: "Por favor, informe seu telefone",
+        endereco: "Por favor, informe seu endereço",
+        cep: "Por favor, informe seu CEP",
+        cpf: "Por favor, informe seu CPF"
+    },
+    submitHandler: function (form) {
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+        form.reset();
+    },
+    invalidHandler: function (event, validator) {
+        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    }
+});
